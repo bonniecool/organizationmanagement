3d0524c7f77a32e6aa8262ab50bd93ce0292ad3d
Kent Van Lois L. Katou <kentvanloisl.katou@Kents-MacBook-Air.local>
2019-06-18 14:15:17 +0800

    create total attendees per month and top attendees dashboard in branch administrator

--- service/app/Modules/Dashboard/Routes/mng/brc/dashboard/index.php
+++ service/app/Modules/Dashboard/Routes/mng/brc/dashboard/index.php
@@ -5,4 +5,6 @@ Route::group(['middleware' => ['BranchAdministrator']], function () {
     Route::get('/member_per_gender', 'Mng\Brc\DashboardController@membersPerGender');
     Route::get('/member_per_user', 'Mng\Brc\DashboardController@membersPerUserType');
     Route::get('/new_member', 'Mng\Brc\DashboardController@newMember');
+    Route::get('/top_attendees', 'Mng\Brc\DashboardController@topAttendees');
+    Route::get('/attendees_per_month', 'Mng\Brc\DashboardController@attendeesPerMonth');
 });
